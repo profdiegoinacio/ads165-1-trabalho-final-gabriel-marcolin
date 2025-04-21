a4789a68dd6b208b0e7847ec80f8ba2a2d5d967a
Gabriel <gabrielgmarcolin12@gmail.com>
2025-04-21 13:39:54 -0300

    Criado DTO dos serviços

---
+++ backend/src/main/java/com/example/dto/ServicoDTO.java
@@ -0,0 +1,88 @@
+package com.example.dto;
+
+import jakarta.validation.constraints.*;
+
+public class ServicoDTO {
+
+    private Long id;
+
+    @NotBlank(message="O título do serviço não pode ser nulo")
+    @Size(min = 3, max = 50, message = "O título deve ter entre 3 e 50 caracteres.")
+    private String titulo;
+
+    @NotBlank(message="A descrição não pode ser nula")
+    @Size(min=3, max = 300, message="A descrição deve ter entre 3 e 300 caracteres.")
+    private String descricao;
+
+    @NotBlank(message="A categoria não pode ser nula")
+    @Size(min=3, max = 50, message="A categoria deve ter entre 3 e 50 caracteres.")
+    private String categoria;
+
+    @NotNull(message = "O preço não pode ser nulo.")
+    @Min(value = 0, message = "O preço deve ser maior ou igual a zero.")
+    private Double preco;
+
+    @NotBlank(message = "O telefone de contato não pode ser nulo.")
+    @Max(value = 11, message = "O telefone inserido é muito grande")
+    private String telefone;
+
+    public ServicoDTO(Long id, String titulo, String descricao, String categoria, Double preco, String telefone) {
+        this.id = id;
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.categoria = categoria;
+        this.preco = preco;
+        this.telefone = telefone;
+    }
+
+    public ServicoDTO() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
