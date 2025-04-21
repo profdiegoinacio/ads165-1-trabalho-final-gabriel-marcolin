a2144ae22fb7c48606d689241e52d9ebd78172d1
Gabriel <gabrielgmarcolin12@gmail.com>
2025-04-21 13:55:37 -0300

    Criado DTO das avaliações

---
+++ backend/src/main/java/com/example/dto/AvaliacaoDTO.java
@@ -0,0 +1,88 @@
+package com.example.dto;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.Date;
+
+public class AvaliacaoDTO {
+    private Long id;
+    private Long servicoId;
+    private Long usuarioId;
+
+    @NotNull(message = "O preço não pode ser nulo.")
+    @Min(value = 1, message = "A nota deve ser maior ou igual a 1.")
+    @Max(value=10, message = "A nota deve ser menor ou igual a 10")
+    private int nota;
+
+    private String comentario;
+    private Date data;
+
+    public AvaliacaoDTO(Long id, Long servicoId, Long usuarioId, int nota) {
+        this.id = id;
+        this.servicoId = servicoId;
+        this.usuarioId = usuarioId;
+        this.nota = nota;
+    }
+
+    public AvaliacaoDTO(Long id, Long servicoId, Long usuarioId, int nota, String comentario, Date data) {
+        this.id = id;
+        this.servicoId = servicoId;
+        this.usuarioId = usuarioId;
+        this.nota = nota;
+        this.comentario = comentario;
+        this.data = data;
+    }
+
+    public AvaliacaoDTO() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getServicoId() {
+        return servicoId;
+    }
+
+    public void setServicoId(Long servicoId) {
+        this.servicoId = servicoId;
+    }
+
+    public Long getUsuarioId() {
+        return usuarioId;
+    }
+
+    public void setUsuarioId(Long usuarioId) {
+        this.usuarioId = usuarioId;
+    }
+
+    public int getNota() {
+        return nota;
+    }
+
+    public void setNota(int nota) {
+        this.nota = nota;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+}
