c8fc9db5790e5f272a4d3bc23ecd1241ea0e6ea7
Gabriel <gabrielgmarcolin12@gmail.com>
2025-04-21 15:58:34 -0300

    Criado gerador de IDs

---
+++ backend/src/main/java/com/example/backend/utils/GeradorDeId.java
@@ -0,0 +1,14 @@
+package com.example.backend.utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class  GeradorDeId{
+    private static Map<String, Long> ids = new HashMap<>();
+
+    public static synchronized long gerarId(String entidade) {
+        Long idAtual = ids.getOrDefault(entidade, 1L);
+        ids.put(entidade, idAtual + 1);
+        return idAtual;
+    }
+}
