c8875e6a80722e037ff91b1b6211314b960bdc02
Gabriel-Marcolinn <gabrielgmarcolin12@gmail.com>
2025-04-28 21:32:12 -0300

    Criada configuração do CORS para permitir a comunicação dos dados entre diferentes portas HTTP

---
+++ backend/src/main/java/com/example/backend/config/CorsConfig.java
@@ -0,0 +1,18 @@
+package com.example.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer{
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")  // Permite todas as rotas
+                .allowedOrigins("http://localhost:3000")  // Permite o frontend rodando em localhost:3000
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")  // Permite esses métodos
+                .allowedHeaders("*")  // Permite todos os cabeçalhos
+                .allowCredentials(true);  // Permite credenciais como cookies ou headers de autenticação
+    }
+}
