166a4a3065cd27b67f308b87237284cc5237b45b
Gabriel <gabrielgmarcolin12@gmail.com>
2025-05-03 16:27:17 -0300

    Criadas mais funções de CRUD para os serviços

--- frontend/src/api/fetchServicos.ts
+++ frontend/src/api/fetchServicos.ts
@@ -11,3 +11,60 @@ export async function fetchServicoById(id: string | Array<string> | undefined) {
         return null;
     }
 }
+
+export async function fetchTodosServicos() {
+    try {
+        const resposta = await fetch(`http://localhost:8080/servicos`);
+        if (!resposta.ok) {
+            throw new Error("Erro ao buscar serviços");
+        }
+        return await resposta.json();
+    } catch (erro) {
+        console.error("Erro ao buscar serviços:", erro);
+        return null;
+    }
+}
+
+export async function criarServico(servico: any) {
+    const resposta = await fetch("http://localhost:8080/servicos", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(servico),
+    });
+
+    if (!resposta.ok) {
+        throw new Error("Erro ao criar serviço");
+    }
+
+    return resposta.json();
+}
+
+export async function removerServico(id: number) {
+    const resposta = await fetch(`http://localhost:8080/servicos/${id}`, {
+        method: "DELETE",
+    });
+
+    if (!resposta.ok) {
+        throw new Error("Erro ao remover serviço");
+    }
+
+    return resposta.text();
+}
+
+export async function EditarServico(servicoAtualizado:any) {
+    const resposta = await fetch("http://localhost:8080/servicos", {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(servicoAtualizado),
+    });
+
+    if (!resposta.ok) {
+        throw new Error("Erro ao Editar serviço");
+    }
+
+    return resposta.json();
+}
