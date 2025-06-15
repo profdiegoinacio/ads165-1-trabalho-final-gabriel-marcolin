161b123a2df7cf72e446621de5f11cb762ca7be3
Gabriel <gabrielgmarcolin12@gmail.com>
2025-06-15 15:19:14 -0300

    Adicionados métodos novos de consulta à API dos usuários

--- frontend/src/api/fetchUsuarios.ts
+++ frontend/src/api/fetchUsuarios.ts
@@ -12,3 +12,48 @@ export async function fetchUsuarioById(id: string | Array<string> | undefined) {
         return null;
     }
 }
+
+export async function fetchUsuarios() {
+    try {
+        const resposta = await fetch(`http://localhost:8080/usuarios`);
+        if (!resposta.ok) {
+            throw new Error("Erro ao buscar usuários");
+        }
+
+        return await resposta.json();
+    } catch (erro) {
+        console.error("Erro ao buscar usuários:", erro);
+        return null;
+    }
+}
+
+export async function fetchUsuarioByUsername(username: string | Array<string> | undefined) {
+    try {
+        const resposta = await fetch(`http://localhost:8080/usuarios/nome/${username}`);
+        console.log('Buscando usuário na URL:', `http://localhost:8080/usuarios/nome/${username}`);
+        if (!resposta.ok) {
+            throw new Error("Erro ao buscar usuário");
+        }
+
+        return await resposta.json();
+    } catch (erro) {
+        console.error("Erro ao buscar usuário:", erro);
+        return null;
+    }
+}
+
+export async function EditarUsuario(usuarioAtualizado:any) {
+    const resposta = await fetch("http://localhost:8080/usuarios", {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(usuarioAtualizado),
+    });
+
+    if (!resposta.ok) {
+        throw new Error("Erro ao Editar usuário");
+    }
+
+    return resposta.json();
+}
